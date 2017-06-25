0e790573f13532c9a897614c887d2e9f7ca637bc
Markus Weber <joop03@alice-dsl.net>
2017-06-25 18:16:28 +0200

    updated Assembly version and fixed null reference exception

--- Source/AssemblyInfo.cs
+++ Source/AssemblyInfo.cs
@@ -9,9 +9,10 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Helix Toolkit")]
 [assembly: AssemblyCompany("Helix Toolkit")]
-[assembly: AssemblyCopyright("Copyright (C) Helix Toolkit 2010-2014.")]
+[assembly: AssemblyCopyright("Copyright (C) GrabMind 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2014.2.1.1")]
-[assembly: AssemblyFileVersion("2014.2.1.1")]
+[assembly: AssemblyVersion("2017.2.1.1")]
+[assembly: AssemblyFileVersion("2017.2.1.1")]
+[assembly: AssemblyInformationalVersion("2017.2.1.1")]
